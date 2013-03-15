05002234b3842ce6fed835fc5eaa839218049717
Aaron Crowder <aaron@aaroncrowder.com>
2013-03-15 02:51:33 -0600

    Made a lot of changes: 1. Moved the data store from text files to a database 2. Logging a record of each check made to reference later 3. Updated error handling to make sure sites that can't be resolved don't crash the script 4. Lots of other misc stuff

--- status_check.py
+++ status_check.py
@@ -1,40 +1,121 @@
 # Imports
 import requests
 import smtplib
+import MySQLdb as mdb
+import sys
 
 from email.mime.text import MIMEText
 
-# Get our variables ready
-COMMASPACE = ', '
-me = 'FROM-ADDRESS/USERNAME'
-password = 'USER-PASSWORD'
+# Define our functions
+def sendErrorMessages(emails, sitesDown, user, password):
+    msg = MIMEText('These sites are down:\n %s' % ('\n'.join(sitesDown)))
+    msg['Subject'] = 'Sites are down!'
+    
+    msg['From'] = user
+    msg['To'] = ', '.join(emails[0])
+    
+    # Send the message via GMail
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()  
+    server.login(user, password)
+    server.sendmail(user, emails, msg.as_string())
+    server.quit()
+#end sendErrorMessages
 
-with open("emails.txt","U") as e_file, open("sites.txt","U") as s_file:
-    sites = e_file.read().split('\n')
-    emails = s_file.read().split('\n')
+def createConnection(server, username, password, database):
+    con = None
+    
+    try:
+        con = mdb.connect(server, username, password, database)
+    except mdb.Error, e:
+        print "Error %d: %s" % (e.args[0], e.args[1])
+        sys.exit(1)
+    #end try/except
+    
+    return con
+#end createConnection
+
+def closeConnection(con):
+    if(con):
+        con.close()
+    #end if
+#end closeConnection
+
+def queryDatabase(con, query):
+    cur = con.cursor()
+    cur.execute(query)
+    
+    return cur.fetchall()
+#end queryDatabase
+
+# Create connection to MySQL Server
+con = createConnection('localhost', 'DBUSER', 'DBPASSWORD', 'DB')
+if not(con):
+    sys.exit(1)
+#end if
+
+# Get our variables ready
+user = queryDatabase(con, 
+    """
+    SELECT 
+        settingValue 
+    FROM 
+        siteSettings 
+    WHERE 
+        settingName = 'email_user'
+    """)[0][0]
+password = queryDatabase(con, 
+    """
+    SELECT 
+        settingValue 
+    FROM 
+        siteSettings 
+    WHERE 
+        settingName = 'email_password'
+    """)[0][0]
+
+sitesDown = []
+sites = queryDatabase(con, "SELECT * FROM websites")
+emails = queryDatabase(con, "SELECT email FROM users WHERE fk_roleid = 1")
 
 # Loop over the sites
 for site in sites:
-    print 'Checking to make sure %s is still up... ' % (site)
-    
-    r = requests.get(site)
-    
-    if(r.status_code != requests.codes.ok):
-        print 'Site is not Ok!'
+    print 'Checking %s ... ' % (site[1])
+    status = 0
+    try:
+        r = requests.get(site[2])
         
-        msg = MIMEText('The site %s is down!' % (site))
-        msg['Subject'] = 'Site is down!'
+        if(r.status_code != requests.codes.ok):
+            sitesDown.append(site[1])
+        else:
+            status = 1
+        #end if
+    except:
+        print 'There was an error, assuming site is down.'
+        sitesDown.append(site[1])
+    finally:
+        if(status):
+            print 'Site is ok!'
+        else:
+            print 'Site is not ok!'
+        #end if
         
-        msg['From'] = me
-        msg['To'] = COMMASPACE.join(emails)
+        queryDatabase(con, 
+            """
+            INSERT
+            INTO websitesChecked(fkid_website, status)
+            VALUES(%d, %d)
+            """ % (int(site[0]), status))
         
-        # Send the message via GMail
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.starttls()  
-        server.login(me, password)
-        server.sendmail(me, emails, msg.as_string())
-        server.quit()
-    else:
-        print 'Site is Ok!'
-    #end if
+        con.commit()
+    #end try/except
 #end for
+
+if(len(sitesDown) > 0):
+    sendErrorMessages(emails, sitesDown, user, password)
+else:
+    print 'All sites where up! Good work!'
+#end if
+
+# Close connection to MySQL Server
+closeConnection(con)
